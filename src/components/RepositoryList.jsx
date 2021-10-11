a344d43dd4ded563d7041e8c76737e132a4a087b
yosoymath <matheusmat170@gmail.com>
2021-10-11 19:04:58 -0300

    Utilizando SASS

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in ReactJS</p>
+                </li>
+            </ul>
+        </section>
+    );
+}
