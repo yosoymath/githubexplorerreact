0116d7e9556e15f02e3aa19a8a9859caca681bcc
yosoymath <matheusmat170@gmail.com>
2021-10-21 23:57:03 -0300

    Listando Repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,13 +2,6 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in ReactJS',
-    link: 'https://github.com/yosoymath/githubexplorerreact'
-}
-
 export function RepositoryList(){
 
     const [repositories, setRepositories] = useState([]);
@@ -26,10 +19,9 @@ export function RepositoryList(){
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem  key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );
